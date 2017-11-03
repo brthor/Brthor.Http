76fc95660ad3c38af53acf13cdbea9c1e5c216b6
Bryan Thornbury <bt.mozoby@gmail.com>
2017-11-03 11:23:42 -0700

    fix up the publish script for this library

--- publish.sh
+++ publish.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -ev
 
-echo "${TRAVIS_PULL_REQUEST}"
-echo "$TRAVIS_PULL_REQUEST_BRANCH"
-echo "$TRAVIS_BRANCH"
+package_name="Brthor.Http"
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo "Publishing Brthor.Http $(printf %05d $TRAVIS_BUILD_NUMBER)"
+    echo "Publishing ${package_name} $(printf %05d $TRAVIS_BUILD_NUMBER)"
 
     dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
-    dotnet nuget push ./bin/Release/Brthor.Http.*.nupkg -s https://www.myget.org/F/thor/api/v2/package -k $MYGET_KEY
+    dotnet nuget push ./bin/Release/${package_name}*.nupkg -k $NUGET_KEY
 fi
